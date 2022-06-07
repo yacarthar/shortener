63e2e1d6f0890697b94b9396f189619146421671
yacarthar <hoang.pham.vbhp@gmail.com>
2022-06-07 19:32:47 +0700

    appengine to Cloud SQL

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ load_dotenv(ENV_FILE)
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_ENV = os.environ.get('FLASK_ENV')
-    
+
     # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
